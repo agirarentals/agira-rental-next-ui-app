ec49072a433d44b9f1cf313c50e1c56d7fea5228
agirarentals <agirarentals@gmail.com>
2025-01-06 23:52:34 +0530

    agira rentals v7 with categories ui and image showing

--- app/admin/categories/components/Form.jsx
+++ app/admin/categories/components/Form.jsx
@@ -1,16 +1,33 @@
 import { Button } from "@nextui-org/react";
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [data,setData]=useState(null);
+  const [image,setImage]=useState(null);
+
+  const handleData=(key,value)=>{
+    setData((preData)=>{
+      return{
+        ...(preData??{}),
+        [key]:value,
+      }
+    })
+  }
   return (
     <div className="bg-white rounded-xl p-6 md:w-[400px]">
       <h1 className="font-semibold">Create Category</h1>
-      <form action="" className="flex flex-col gap-3">
+      <form onSubmit={e=>e.preventDefault()} action="" className="flex flex-col gap-3">
         <div className="flex flex-col gap-1">
           <label htmlFor="category-name" className="text-gray-500 text-sm">
             Image <span className="text-red-500">*</span>
           </label>
+          {image && <div className="flex justify-center items-center p-3"><img className="h-32" src={URL.createObjectURL(image)} alt="image" /></div>}
           <input
+            onChange={e=>{
+              if(e.target.files.length>0){
+                setImage(e.target.files[0])
+              }
+            }}
             type="file"
             name="category-image"
             id="category-image"
@@ -26,6 +43,8 @@ const Form = () => {
             placeholder="Enter Name"
             name="category-name"
             id="category-name"
+            value={data?.name??""}
+            onChange={e=>handleData('name',e.target.value)}
             className="border px-4 py-2 rounded-lg w-full focus:outline-none"
           />
         </div>
@@ -38,10 +57,12 @@ const Form = () => {
             placeholder="Enter URL"
             name="category-url"
             id="category-url"
+            value={data?.url??""}
+            onChange={e=>handleData('url',e.target.value)}
             className="border px-4 py-2 rounded-lg w-full focus:outline-none"
           />
         </div>
-        <Button>Create</Button>
+        <Button type="submit">Create</Button>
       </form>
     </div>
   );
